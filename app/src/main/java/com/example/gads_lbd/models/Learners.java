8e2761210caffbf554ab9e1a0e4a466d1c1726eb
ShadKobimbo <shadkobimbo@outlook.com>
2020-09-11 22:51:39 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/gads_lbd/models/Learners.java
@@ -0,0 +1,12 @@
+package com.example.gads_lbd.models;
+
+public class Learners {
+
+    private String learner_name;
+    private String learner_hours;
+    private String learner_country;
+    private String learner_badge;
+
+    
+
+}
