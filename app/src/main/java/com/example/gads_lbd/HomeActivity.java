e5cae96f9deca0c52998a31c6386a0ae2c1fb592
ShadKobimbo <shadkobimbo@outlook.com>
2020-09-11 22:37:16 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/gads_lbd/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.gads_lbd;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.gads_lbd.adapters.SectionsPageAdapter;
+import com.example.gads_lbd.ui.LearningFragment;
+import com.example.gads_lbd.ui.SkillsFragment;
+import com.google.android.material.tabs.TabLayout;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private SectionsPageAdapter mSectionsPageAdapter;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new LearningFragment(), "Learning Leaders");
+        adapter.addFragment(new SkillsFragment(), "Skill IQ Leaders");
+        viewPager.setAdapter(adapter);
+
+    }
+}
