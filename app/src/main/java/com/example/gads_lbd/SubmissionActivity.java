f0860f606ca13d5b4078bec31044b795456b8e81
ShadKobimbo <shadkobimbo@outlook.com>
2020-09-12 15:12:24 +0300

    Initial Commit

--- app/src/main/java/com/example/gads_lbd/SubmissionActivity.java
+++ app/src/main/java/com/example/gads_lbd/SubmissionActivity.java
@@ -7,21 +7,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class SubmissionActivity extends AppCompatActivity {
 
     static Context context = null;
     Dialog dialog;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+
+        context = this;
+        submit = (Button) findViewById(R.id.submit_project);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAreYouSureDialog();
+            }
+        });
+    }
+
+    public void trySubmit(){
+
+
     }
 
     public void back(View view){
-
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
 
@@ -30,24 +46,20 @@ public class SubmissionActivity extends AppCompatActivity {
     public void openAreYouSureDialog() {
         dialog = new Dialog(context); // Context, this, etc.
         dialog.setContentView(R.layout.dialog_confirm);
-        dialog.setTitle(R.string.user_name);
         dialog.show();
     }
     public void openSuccessDialog() {
         dialog = new Dialog(context); // Context, this, etc.
         dialog.setContentView(R.layout.dialog_success);
-        dialog.setTitle(R.string.user_name);
         dialog.show();
     }
     public void openFailedDialog() {
         dialog = new Dialog(context); // Context, this, etc.
         dialog.setContentView(R.layout.dialog_failed);
-        dialog.setTitle(R.string.user_name);
         dialog.show();
     }
 
     public void cancel(View view) {
-        Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_LONG).show();
         dialog.cancel();
     }
 }
